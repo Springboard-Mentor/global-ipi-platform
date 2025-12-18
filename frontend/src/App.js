1a8ad3c4d897dbb7dd8a6de2a0227c9c785ba7fc
Ritika <ritika1905kumari@gmail.com>
2025-12-18 16:29:30 +0530

    organized project folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import Login from './components/Login';
-import Register from './components/Register';
-import Dashboard from './components/Dashboard.jsx';
-import Profile from './components/Profile';
-import IPActivity from './components/IPActivity.js';
-import HelpCenter from './components/HelpCenter';
-import PrivacyPolicy from './components/PrivacyPolicy';
-import TermsOfService from './components/TermsOfService';
-import Settings from './components/Settings';
-import Feedback from './components/Feedback';
-import IPSearch from './components/IPSearch';
-import SearchResults from './components/SearchResults';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard.jsx";
+import Profile from "./components/Profile";
+import IPActivity from "./components/IPActivity.js";
+import HelpCenter from "./components/landingPageComponents/HelpCenter";
+import PrivacyPolicy from "./components/landingPageComponents/PrivacyPolicy.js";
+import TermsOfService from "./components/landingPageComponents/TermsOfService.js";
+import Settings from "./components/dashboardComponents/Settings";
+import Feedback from "./components/landingPageComponents/Feedback";
+import IPSearch from "./components/IPSearch";
+import SearchResults from "./components/dashboardComponents/ipSearchComponents/SearchResults.js";
 
 function App() {
   return (
-      <div className="App">
-        <Routes>
+    <div className="App">
+      <Routes>
         <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/ipActivity" element={<IPActivity />} />
-          <Route path="/help" element={<HelpCenter />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/terms" element={<TermsOfService />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/ip-search" element={<IPSearch />} />
-          <Route path="/search-results" element={<SearchResults />} />
-        </Routes>
-      </div>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/ipActivity" element={<IPActivity />} />
+        <Route path="/help" element={<HelpCenter />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsOfService />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/ip-search" element={<IPSearch />} />
+        <Route path="/search-results" element={<SearchResults />} />
+      </Routes>
+    </div>
   );
 }
 
