80b3ac2bb1fba4890b1977ea3a487455fc75fb04
Ritika <ritika1905kumari@gmail.com>
2026-01-19 02:55:02 +0530

    add Monitoring tools navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,6 +32,7 @@ import AdminDashboard from "./admin/pages/AdminDashboard";
 import AdminFilingDetail from "./admin/pages/AdminFilingDetail";
 import AdminUserManagement from "./admin/pages/AdminUserManagement";
 import AdminFilingsManagement from "./admin/pages/AdminFilingsManagement";
+import AdminMonitoring from "./admin/pages/AdminMonitoring";
 import AdminFinanceManagement from "./admin/pages/AdminFinanceManagement";
 import AdminSettings from "./admin/pages/AdminSettings";
 
@@ -47,6 +48,7 @@ function App() {
           <Route path="/admin/users" element={<AdminUserManagement />} />
           <Route path="/admin/filings" element={<AdminFilingsManagement />} />
           <Route path="/admin/filings/:id" element={<AdminFilingDetail />} />
+          <Route path="/admin/monitoring" element={<AdminMonitoring />} />
           <Route path="/admin/finance" element={<AdminFinanceManagement />} />
           <Route path="/admin/settings" element={<AdminSettings />} />
           <Route path="/register" element={<Register />} />
